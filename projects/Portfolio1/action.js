929ed5a21a06feade26734b1617524afe1cb6e95
Connor Ruggles <cruggles@iastate.edu>
2016-09-26 18:27:07 -0500

    Add files via upload

--- projects/Portfolio1/action.js
+++ projects/Portfolio1/action.js
@@ -1,32 +1,14 @@
 var map;
-var saltLogo = '../Images/salt_logo_small.jpg';
-var marksFace = new google.maps.MarkerImage('../Images/salt_teal_white_small.png',
+var marksFace = new google.maps.MarkerImage('../Images/salt_teal_white_border.png',
     new google.maps.Size(40, 40),
     new google.maps.Point(0, 0),
     new google.maps.Point(20, 20));
 var userLocation;
 var geocoder;
 var apiKey = "AIzaSyB2mnvZqIZXdQYIy7jZu31JQLnhhgKFjJ4";
-
-var sheetSrc = "https://docs.google.com/spreadsheets/d/1UlBlLoto8QNT3558MwLjgbcvuhQUH9GXQmspBaoVaJ8/edit?ts=57e15ae1#gid=2042134768";
-
-/**
- * Used for authentication
- */
-var CLIENT_ID = '769872060184-gon53at54dmqn7h54t4c38od6sru3nph.apps.googleusercontent.com';
-var SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-
-/* API key to access Google Sheets API with account whiterun.thane753@gmail.com */
-var sheetsAPIKey = 'AIzaSyA2fSUmxTZAp0y5I5GXWP2c30WIFIFyxBo';
-
-/* API key to access Google Sheets with account cruggles@iastate.edu */
-var rugglesSheetsAPIKey = 'AIzaSyCk-GAaktP5Qf2WwrNnKOVCdQWtuExhOpU';
-
-/* Same deal for this client ID */
-var rugglesClientID = '454351574206-g5v6n65tmltp64elhj7jnl9qtecra57s.apps.googleusercontent.com';
-
-/* ID for the spreadsheet */
-var spreadsheetID = '1UlBlLoto8QNT3558MwLjgbcvuhQUH9GXQmspBaoVaJ8';
+var modal;
+var span;
+var btn;
 
 function initializeISU() {
     initialize(42.027005, -93.646661, 15);
@@ -103,6 +85,7 @@ var listener1 = google.maps.event.addDomListener(window, 'load', function() {
     initializeISU();
 });
 
+//Centers the map to show the desied location
 function goToCurrentLocation() {
     map.setCenter(userLocation.position);
 }
@@ -152,17 +135,84 @@ function goToGreek() {
     cGroupInfo("Greek");
 }
 
-// var enteredAddressField;
-// $(docuemnt).ready(function() {
-//     enteredAddressField = document.getElementById("entered-address");
-//     enteredAddressField.addEventListener("keydown", function(event) {
-//         if (event.keyCode == 13) {
-//             goToEnteredAddress();
-//         }
-//     });
-// });
-
 window.onload = function() {
+    modal = document.getElementById('signup');
+    span = document.getElementById("close-modal");
+    submit = document.getElementById('submit-modal');
+    btn = document.getElementById("btn1");
+    selectYear = document.getElementById("year");
+    btn.onclick = openModal;
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var gender = document.getElementById("gender");
+    var location = document.getElementById("location");
+    var year = document.getElementById("year");
+    var explain = document.getElementById("other-explanation");
+
+    function validColor(obj) {
+        if (obj.validity.valid) {
+            obj.style.boxShadow = "none";
+            obj.style.border = "1px solid #ABABAB";
+        } else {
+            obj.style.boxShadow = "0 0 5px rgba(255, 0, 0, 1)";
+            obj.style.border = "1px solid rgba(255, 0, 0, 1)";
+        }
+    }
+
+    name.onchange = function() {
+        validColor(name);
+    }
+    email.onchange = function() {
+        validColor(email);
+    }
+    phone.onchange = function() {
+        validColor(phone);
+    }
+    gender.onchange = function() {
+        validColorDrop(gender);
+    }
+    location.onchange = function() {
+        validColorDrop(location);
+    }
+    year.onchange = function() {
+        validColorDrop(year);
+    }
+    explain.onchange = function() {
+        validColor(explain);
+    }
+
+    function validColorDrop(obj) {
+        if (obj.value == "") {
+            obj.style.border = "1px solid red";
+            obj.style.boxShadow = "0 0 5px rgba(255, 0, 0, 1)";
+        } else {
+            obj.style.border = "1px solid #ABABAB";
+            obj.style.boxShadow = "none";
+        }
+    }
+
+    selectYear.onchange = function() {
+        if (selectYear.value == "other") {
+            document.getElementById("otherDiv").style.display = "block";
+        } else {
+            document.getElementById("otherDiv").style.display = "none";
+        }
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    submit.onclick = function() {
+        checkValid();
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
     document.querySelector('#entered-address').addEventListener('keypress', function(e) {
         var key = e.which || e.keyCode;
         if (key === 13) {
@@ -170,7 +220,54 @@ window.onload = function() {
             goToEnteredAddress();
         }
     })
-    pullData();
+
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    function val(obj, bool) {
+        if (!obj.validity.valid) {
+            bool = false;
+        }
+        return bool;
+    }
+
+    function valDropDown(obj, bool) {
+        if (obj.value == "") {
+            obj.style.bocShadow = "0 0 5px red";
+            obj.style.border = "red";
+            bool = false;
+        }
+        return bool;
+    }
+
+    function validate() {
+        var isValid = true;
+        namebool = val(name, isValid);
+        emailbool = val(email, isValid);
+        phonebool = val(phone, isValid);
+        genderbool = valDropDown(gender, isValid);
+        locbool = valDropDown(location, isValid);
+        yearbool = valDropDown(year, isValid);
+        explainbool = true;
+        if (year.value == "other" && explain.value == "") {
+            explainbool = false;
+            explain.style.border = "1px solid red";
+            explain.style.boxShadow = "0 0 5px red";
+        }
+        if ((namebool && emailbool && phonebool && genderbool && locbool && yearbool && explainbool) == false) {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    function checkValid() {
+        var bool = validate();
+        if (bool) {
+            //TODO - the information within the modal must be transfered into the database
+            modal.style.display = "none";
+        }
+    }
 }
 
 function goToEnteredAddress() {
@@ -189,200 +286,13 @@ function goToEnteredAddress() {
     });
 }
 
+//Changes the text above the side panel to say "C-Groups near " selected location/address
 function cGroupInfo(location) {
     var temp = "C-Groups Near " + location;
     document.getElementById("side-panel-title").innerHTML = temp;
 }
 
-// function getLeaders(cgroup) {
-//     gapi.client.sheets.spreadsheets.values.get({
-//         spreadsheetId: spreadsheetID,
-//         range: 'cgroups!A1:B10',
-//     }).then(function(response) {
-//         var range = response.result;
-//         if(range.values.length > 0) {
-//             for(i = 0; i < range.values.length; i++) {
-//                 var row = range.values[i];
-//                 cgroup.addLeader(row[0]);
-//                 cgroup.addLeader(row[1]);
-//             }
-//         }
-//     });
-// }
-
-/**
- * Check if current user has authorized this application.
- */
-function checkAuth() {
-  gapi.auth.authorize(
-    {
-      'client_id': CLIENT_ID,
-      'scope': SCOPES.join(' '),
-      'immediate': true
-    }, handleAuthResult);
-}
-
-/**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
-function handleAuthResult(authResult) {
-  var authorizeDiv = document.getElementById('authorize-div');
-  if (authResult && !authResult.error) {
-    // Hide auth UI, then load client library.
-    authorizeDiv.style.display = 'none';
-    loadSheetsApi();
-  } else {
-    // Show auth UI, allowing the user to initiate authorization by
-    // clicking authorize button.
-    authorizeDiv.style.display = 'inline';
-  }
-}
-
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
-function handleAuthClick(event) {
-  gapi.auth.authorize(
-    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-    handleAuthResult);
-  return false;
-}
-
-/**
-* Load Sheets API client library.
-*/
-function loadSheetsApi() {
-    var discoveryUrl =
-        'https://sheets.googleapis.com/$discovery/rest?version=v4';
-    gapi.client.load(discoveryUrl);
-}
-
-var allCGroups = new Array();
-
-/**
- * Grabs entire Google Sheet, and creates a cgroup
- * for every row in the sheet
- */
-function pullData() {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: spreadsheetID,
-        range: 'cgroups!A1:H',
-    }).then(function(response) {
-        var range = response.result;
-        if(range.values.length > 0) {
-            for(i = 0; i < range.values.length; i++) {
-                var row = range.values[i];
-                var tmpGroup = new cGroup();
-                tmpGroup.addLeader(row[0]);
-                tmpGroup.addLeader(row[1]);
-                tmpGroup.addEmail(row[2]);
-                tmpGroup.addPhone(row[3]);
-                tmpGroup.addTime(row[4]);
-                tmpGroup.addLoc(row[5]);
-                tmpGroup.addArea(row[6]);
-
-                allCGroups[i] = tmpGroup;
-
-                document.getElementById('location-time').innerHTML = tmpGroup.time;
-                document.getElementById('leader-names').innerHTML = tmpGroup.leaders[0] + '/' + tmpGroup.leaders[1];
-                document.getElementById('address').innerHTML = tmpGroup.location;
-            }
-        }
-    });
-}
-
-/**
- * Creates a cGroup() object and defines function properties.
- * All initialization is done in the constructor, no parameters
- * need to be passed.
- */
-function cGroup() {
-    // array of Strings for names of leaders
-    this.leaders = new Array();
-    // String for location. We could separate this a little into
-    // street, city, state, ZIP
-    this.location = "";
-    // contact email
-    this.email = "";
-    // contact phone number
-    this.phone = "";
-    // also a String
-    this.time = "Monday, 7:00";
-    // number of members, someone joining this group 
-    // will just increment by 1
-    this.numMembers = 0;
-
-    this.area = "";
-
-    this.addArea = function(area) {
-        this.area = area;
-    };
-
-    /**
-     * Sets email
-     * 
-     * @param {string} contact email
-     */
-    this.addEmail = function(email) {
-        this.email = email;
-    };
-
-    /**
-     * Sets phone number
-     * 
-     * @param {string} contact phone number
-     */
-    this.addPhone = function(phoneNumber) {
-        this.phone = phoneNumber;
-    };
-
-    /**
-     * Sets time for this group
-     * 
-     * @param {string} time that the group will be meeting
-     */
-    this.addTime = function(time) {
-        this.time = time;
-    };
-
-    /**
-     * Sets this location to 'loc'
-     * 
-     * @param {string}
-     */
-    this.addLoc = function(loc) {
-        this.location = loc;
-    };
-
-    /**
-     * Just returns the array of leaders, mostly for debugging purposes
-     */
-    this.getLeaders = function() {
-        return this.leaders;
-    };
-
-    /**
-     * Adds the leader to the leader array
-     * 
-     * @param {string}
-     */
-    this.addLeader = function(leader) {
-        this.leaders[this.leaders.length] = leader;
-        // this.numLeaders++;
-    };
-
-    /**
-     * Pretty obvious what this does.
-     */
-    this.addMember = function(){numMembers++;};
-}
-
-
-
+//initializes C-Groups that exist on campus in known and consistant locations from year to year
 function initializeCGroups() {
     initLinden();
     initMaple();
@@ -519,13 +429,18 @@ function initBuchanan() {
 
 }
 
-function generateCGroupPanel() {
-    var html = '<div class="cgroup-panel"><h5 id="location-time"></h5><button type="button" class="join-button pull-right">Join</button><p id="leader-names"></p><p id="address"></p></div>';
+//TODO unfinished but should take a c-group object in and add a C-Group panel into the side panel displaying the relevant information about the C-Group
+function generateCGroupPanel(cgObject) {
+    var html = '<div class="cgroup-panel"><h5 id="location-time">' + cgObject.location + cgObject.time + '</h5><button type="button" class="join-button pull-right" id="cgroup' + cgObject.ID + '">Join</button><p id="leader-names">' + leaderToString(cgObject) + '</p><p id="address">' + cgObject.address + '</p></div>';
     var panel = document.getElementById("side-panel");
     panel.insertAdjacentHTML("beforeend", html);
 }
 
-// function createCGroup(location, leader1, leader2) {
-
-//     generateCGroupPanel(location);
-// }
+//Creates a string with all of the leaders for a given C-Group
+function leaderToString(cgObject) {
+    var leaderString = cgObject.leader[0];
+    for (i = 1; i < cgObject.leader.length(); i++) {
+        leaderString += '/' + cgObject.leader[i];
+    }
+    return leaderString;
+}
